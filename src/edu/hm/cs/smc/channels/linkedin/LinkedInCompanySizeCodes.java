3cb8a860b1dd2f016c096b303f8c2c282cc59f2a
Max Keller <keller6@hm.edu>
2016-11-24 14:27:00 +0100

    LinkedIn: Added Company Size Codes.

---
+++ src/edu/hm/cs/smc/channels/linkedin/LinkedInCompanySizeCodes.java
@@ -0,0 +1,53 @@
+package edu.hm.cs.smc.channels.linkedin;
+
+/**
+ * Codes that are used by LinkedIn to identify specific specific company sizes.
+ *
+ */
+public class LinkedInCompanySizeCodes {
+	
+	/**
+	 * Self-employed
+	 */
+	public static final String A = "A";
+	
+	/**
+	 * 1-10 employees
+	 */
+	public static final String B = "B";
+	
+	/**
+	 * 11-50 employees
+	 */
+	public static final String C = "C";
+
+	/**
+	 * 51-200 employees
+	 */
+	public static final String D = "D";
+
+	/**
+	 * 201-500 employees
+	 */
+	public static final String E = "E";
+
+	/**
+	 * 501-1000 employees
+	 */
+	public static final String F = "F";
+
+	/**
+	 * 1001-5000 employees
+	 */
+	public static final String G = "G";
+
+	/**
+	 * 5001-10,000 employees
+	 */
+	public static final String H = "H";
+
+	/**
+	 * 10,001+ employees
+	 */
+	public static final String I = "I";
+}
