01024cf197c485ba7d450ddeffb92ef64885955c
Max Keller <keller6@hm.edu>
2016-11-24 14:45:03 +0100

    LinkedIn: Added Job Function Codes.

---
+++ src/edu/hm/cs/smc/channels/linkedin/LinkedInJobfFunctionCodes.java
@@ -0,0 +1,134 @@
+package edu.hm.cs.smc.channels.linkedin;
+
+public class LinkedInJobfFunctionCodes {
+	
+	/**
+	 * Accounting
+	 */
+	public static final String ACCT = "acct";
+	
+	/**
+	 * Administrative
+	 */
+	public static final String ADMN = "admn";
+	
+	/**
+	 * Arts and Design
+	 */
+	public static final String CRE = "cre";
+
+	/**
+	 * Business Development
+	 */
+	public static final String BD = "bd"; 
+
+	/**
+	 * Community & Social Services
+	 */
+	public static final String CSS = "css";
+	
+	/**
+	 * Consulting
+	 */
+	public static final String CNSL = "cnsl";
+	
+	/**
+	 * Education
+	 */
+	public static final String EDU = "edu";
+
+	/**
+	 * Engineering
+	 */
+	public static final String ENG = "eng";
+	
+	/**
+	 * Entrepreneurship
+	 */
+	public static final String ENT = "ent";
+
+	/**
+	 * Finance
+	 */
+	public static final String FINC = "finc";
+
+	/**
+	 * Healthcare Services
+	 */
+	public static final String MED = "med";
+	
+	/**
+	 * Human Resources
+	 */
+	public static final String HR = "hr";
+	
+	/**
+	 * Information Technology
+	 */
+	public static final String IT = "it";
+	
+	/**
+	 * Legal
+	 */
+	public static final String LGL = "lgl";
+	
+	/**
+	 * Marketing
+	 */
+	public static final String MKTG = "mktg";
+
+	/**
+	 * Media & Communications
+	 */
+	public static final String PR = "pr";
+	
+	/**
+	 * Military & Protective Services
+	 */
+	public static final String MPS = "mps";
+
+	/**
+	 * Operations
+	 */
+	public static final String OPS = "ops";
+	
+	/**
+	 * Product Management
+	 */
+	public static final String PROD = "prod";
+	
+	/**
+	 * Program & Product Management
+	 */
+	public static final String PPM = "ppm";
+	
+	/**
+	 * Purchasing
+	 */
+	public static final String BUY = "buy";
+	
+	/**
+	 * Quality Assurance
+	 */
+	public static final String QA = "qa";
+	
+	/**
+	 * Real Estate
+	 */
+	public static final String RE = "re";
+	
+	/**
+	 * Rersearch
+	 */
+	public static final String ACAD = "acad";
+	
+	/**
+	 * Sales
+	 */
+	public static final String SALE = "sale";
+	
+	/**
+	 * Support
+	 */
+	public static final String SUPP = "supp";
+}
